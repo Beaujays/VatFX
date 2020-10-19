877bb8446c5708ec8bd2ac93678ba6d15721e11b
Jabers999 <45258150+Jabers999@users.noreply.github.com>
2020-10-19 20:08:51 +0200

    cLEAN SLATE

--- src/todoRenamePackage/service/ObservableShape.java
+++
@@ -1,9 +0,0 @@
-package todoRenamePackage.service;
-
-import todoRenamePackage.domain.Shape;
-import javafx.collections.ObservableList;
-
-public interface ObservableShape extends ShapeInterface {
-
-    ObservableList<Shape> getObservableList();
-}
